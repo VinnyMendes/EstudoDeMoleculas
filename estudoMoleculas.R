ec6a4f1087f6765be75ff4cca660171911543857
VinnyMendes <vinypotter.vm@gmail.com>
2022-11-30 22:56:28 -0300

    added some last things

--- estudoMoleculas.R
+++ estudoMoleculas.R
@@ -10,6 +10,9 @@ summary(data)
 #quantidade de linhas
 nrow(data)
 
+#primeros 5 valores
+head(data)
+
 #quantidade de colunas
 length(data)
 
@@ -49,18 +52,41 @@ hist(data$num_h_acceptors)
 hist(data$num_h_donors)
 
 #correlacao entre as variaveis
-cor.test(data$exact_mol_wt, data$tpsa) #0.30
-cor.test(data$num_h_acceptors, data$num_h_donors) #0.27
+
 cor.test(data$exact_mol_wt, data$ring_count) #0.55
 cor.test(data$exact_mol_wt, data$num_rotatable_bonds) #0.54
 cor.test(data$exact_mol_wt, data$mol_log_p) #0.57
 cor.test(data$exact_mol_wt, data$num_h_acceptors) #0.42
 cor.test(data$exact_mol_wt, data$num_h_donors) #0.003
+cor.test(data$exact_mol_wt, data$tpsa) #0.30
+
+cor.test(data$ring_count, data$num_rotatable_bonds) #-0.004
+cor.test(data$ring_count, data$mol_log_p) #0.43
+cor.test(data$ring_count, data$num_h_acceptors) #0.15
+cor.test(data$ring_count, data$num_h_donors) #-0.22
+cor.test(data$ring_count, data$tpsa) #0.0009
+
+cor.test(data$num_rotatable_bonds, data$mol_log_p) #0.18
+cor.test(data$num_rotatable_bonds, data$num_h_acceptors) #0.41
+cor.test(data$num_rotatable_bonds, data$num_h_donors) #0.18
+cor.test(data$num_rotatable_bonds, data$tpsa) #0.18
+
+cor.test(data$mol_log_p, data$num_h_acceptors) #-0.29
+cor.test(data$mol_log_p, data$num_h_donors) #-0.27
+cor.test(data$mol_log_p, data$tpsa) #-0.34
+
+cor.test(data$num_h_acceptors, data$num_h_donors) #0.27
+cor.test(data$num_h_acceptors, data$tpsa) #0.92
+
+cor.test(data$num_h_donors, data$tpsa) #0.39
+
 
 #grafico das maiores correlacoes
 plot(data$exact_mol_wt, data$ring_count)
 plot(data$exact_mol_wt, data$num_rotatable_bonds)
 plot(data$exact_mol_wt, data$mol_log_p)
+
+plot(data$num_h_acceptors, data$tpsa)
 
 library(tidyverse)
 
@@ -73,14 +99,16 @@ ggplot(data=data) +
 ggplot(data=data) +
   geom_point(mapping= aes(x=exact_mol_wt, y=mol_log_p))
 
+ggplot(data=data) +
+  geom_point(mapping= aes(x=num_h_acceptors, y=tpsa))
 
 #escolhendo o melhor grafico e adicionando linha de tendencia
 
 ggplot(data = data, mapping = aes(x = exact_mol_wt, y=ring_count)) + geom_point() + geom_smooth()
 
+ggplot(data = data, mapping = aes(x = exact_mol_wt, y=num_rotatable_bonds)) + geom_point() + geom_smooth()
 
+ggplot(data = data, mapping = aes(x = exact_mol_wt, y=mol_log_p)) + geom_point() + geom_smooth()
 
-
-
-
+ggplot(data = data, mapping = aes(x=num_h_acceptors, y=tpsa)) + geom_point() + geom_smooth()
 
